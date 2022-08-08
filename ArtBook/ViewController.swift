b1a829d12d1309cb080c9a663aec26d012b70b63
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-08-08 19:33:36 +0300

    Initial Commit

---
+++ ArtBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBook
+//
+//  Created by Sezer on 8.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
