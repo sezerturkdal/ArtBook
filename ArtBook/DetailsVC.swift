ba73a8cf65636458f075ae14d359e9cfa8ceefae
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-08-21 12:46:08 +0300

    ImagePicker added

--- ArtBook/DetailsVC.swift
+++ ArtBook/DetailsVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
 
     @IBOutlet weak var txtName: UITextField!
@@ -20,11 +20,28 @@ class DetailsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Recognizers
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        
         view.addGestureRecognizer(gestureRecognizer)
         
-        // Do any additional setup after loading the view.
+        artImage.isUserInteractionEnabled=true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        artImage.addGestureRecognizer(imageTapRecognizer)
+    }
+    
+    @objc func selectImage(){
+        let picker=UIImagePickerController()
+        picker.delegate=self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing=true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        artImage.image=info[.editedImage] as? UIImage
+        self.dismiss(animated: true) {
+            print("imagePicker kapatildi")
+        }
     }
     
     @objc func hideKeyboard(){
